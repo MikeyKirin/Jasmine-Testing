c4df65a5f4e5002eb319a7607babc8a782ef6f75
MikeyKirin <101678214+MikeyKirin@users.noreply.github.com>
2023-05-13 23:44:16 -0400

    Update calculator-test.js

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,21 @@
-
-it('should calculate the monthly rate correctly', function () {
+it('should calculate the monthly rate correctly', function() {
   // ...
+  const values = {
+    amount: 100,
+    years: 1,
+    rate: 10
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('9.16');
 });
+
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 5000,
+    years: 3,
+    rate: 0.8
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('150.00');
 });
 
-/// etc
